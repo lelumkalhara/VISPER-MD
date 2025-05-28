033449137c3d5d4aa3c379129271c10c9d9a96d4
lelumkalhara <lelumkalhara44@gmail.com>
2025-05-28 12:04:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '0VRGXC6R#inFb_4Qo9cZ61V2eUMtu1J0w7aKUjHgSakp6yeDdTRY' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'NUAHDBwT#X7j9wbhN4dIRUG_7TYUHjGfUCkj4CV8y_62nghyNE9k' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || ',' ,
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
